c2cff99a357714eaffd06f42a33062076552227b
DumplingLife <dumplinglife7584@gmail.com>
2020-02-17 19:52:22 -0800

    sessionRouter fix

--- routes/sessionRouter.js
+++ routes/sessionRouter.js
@@ -20,25 +20,42 @@ router.post('/request', auth.loggedIn, auth.ensureUserIsClient, function(req, re
 });
 router.post('/rate', auth.loggedIn, auth.ensureUserIsClient, function(req,res) {
   var sessionId = req.body.sessionId;
-  Sessions.findById(sessionId, function(err, session) {
-    if(req.user.sessions.contains(session) && !req.user.ratedSessions.contains(session))
+
+  //basically req.user.sessions.includes
+  var sessionInsideSessions = req.user.sessions.some(function(session) {
+    return session._id == sessionId;
+  });
+  var sessionInsideRatedSessions = req.user.ratedSessions.some(function(session) {
+    return session._id == sessionId;
+  });
+
+  if(sessionInsideSessions && !sessionInsideRatedSessions)
+  {
+    var rating = Math.floor(req.body.rating); //make sure rating is int
+    if(rating>=0 && rating<5)
     {
-      var rating = req.body.rating;
-      if(Number.isInteger(rating) && rating>=0 && rating<5)
-      {
+      Sessions.findById(sessionId, function(err, session) {
         var start = session.date.getTime();
         var end = session.dateCompletedAt.getTime();
         if(end == null) {
           res.send("session has not ended");
         }
         else {
-          var weight = Math.ceil((start-end)/(1000*60*15));
-          req.user.ratings[rating] += weight;
-          req.user.save();
+          var weight = Math.ceil((end-start)/(1000*60*15));
+          console.log(weight);
+          Guides.findById(session.createdBy, function(err, guide) {
+            //Similar to
+            //guide.ratings[rating] += weight;
+            //but marks it as modified. Using ^^^ would not save
+            guide.ratings.set(rating, guide.ratings[rating] + weight);
+            console.log(guide.ratings);
+            guide.save();
+            res.send("success");
+          });
         }
-      }
+      });
     }
-  });
+  }
 });
 
 
