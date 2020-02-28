15bfdeccc75c3ede0b232a1b54fffc8389219b2b
lucasxz <lucasxz@outlook.com>
2020-02-28 00:12:52 -0500

    fixed payrouter not actually sending proper error response

--- routes/payRouter.js
+++ routes/payRouter.js
@@ -8,60 +8,62 @@ router.use(auth.loggedIn);
 router.use(auth.ensureUserIsClient);
 
 router.get('/addpaymentmethod', function(req, res) {
-  res.render('userAddPayment', {layout: false});
+    res.render('userAddPayment', { layout: false });
 });
 
 router.post('/addpaymentmethod', function(req, res, next) {
-  const stripeid = req.user.stripeCustomerId;
-  const token = req.body.stripeToken;
-  stripe.customers.createSource(
-      stripeid,
-      {source: token},
-      function(err, card) {
-        // asynchronously called
-        if (err) next(err);
-        res.redirect('/pay');
-      },
-  );
+    const stripeid = req.user.stripeCustomerId;
+    const token = req.body.stripeToken;
+    stripe.customers.createSource(
+        stripeid, { source: token },
+        function(err, card) {
+            // asynchronously called
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            res.redirect('/pay');
+        },
+    );
 });
 
 router.get('/', function(req, res, next) {
-  const stripeid = req.user.stripeCustomerId;
-  stripe.customers.listSources(
-      stripeid,
-      function(err, cards) {
-        console.log(err)
-        if (err) next(err);
-        res.render('userPay', {layout: false, cards: cards.data});
-      },
-  );
+    const stripeid = req.user.stripeCustomerId;
+    stripe.customers.listSources(
+        stripeid,
+        function(err, cards) {
+            console.log(err)
+            if (err) next(err);
+            res.render('userPay', { layout: false, cards: cards.data });
+        },
+    );
 });
 
 router.post('/', async function(req, res) {
-  const token = req.body.source;
-  console.log(token)
-  const amount = req.body.CreditAmount * 100;
-  stripe.charges.create({
-    amount: amount,
-    currency: 'usd',
-    customer: req.user.stripeCustomerId,
-    source: token,
-    capture: true,
-  },
-  function(err, charge) {
-    console.log(err);
-    if (err) next(err);
-    stripe.customers.createBalanceTransaction(
-        req.user.stripeCustomerId,
-        {amount: -charge.amount, currency: 'usd', description: 'Session refill.'},
-        function(err, customer) {
-          console.log(err);
-          if (err) next(err);
-          res.redirect('/dashboard');
+    const token = req.body.source;
+    console.log(token)
+    const amount = req.body.CreditAmount * 100;
+    stripe.charges.create({
+            amount: amount,
+            currency: 'usd',
+            customer: req.user.stripeCustomerId,
+            source: token,
+            capture: true,
+        },
+        function(err, charge) {
+            console.log(err);
+            if (err) next(err);
+            stripe.customers.createBalanceTransaction(
+                req.user.stripeCustomerId, { amount: -charge.amount, currency: 'usd', description: 'Session refill.' },
+                function(err, customer) {
+                    console.log(err);
+                    if (err) next(err);
+                    res.redirect('/dashboard');
+                },
+            );
         },
     );
-  },
-  );
 });
 
 
