07824c43dce363059832bb91ec1abed294b16042
DumplingLife <dumplinglife7584@gmail.com>
2020-03-29 12:57:04 -0700

    session save document fix
    also some minor front end changes to document

--- api/document.js
+++ api/document.js
@@ -32,7 +32,6 @@ router.put('/:id', auth.loggedIn, auth.hasDocument, function(req, res) {
   });
 });
 router.delete('/:id', auth.loggedIn, auth.hasDocument, function(req, res) {
-  console.log("delete request");
   s3.deleteObject({Bucket: "tutopoint-doc-bucket", Key: req.doc.aws_filename}, function(err, data) {
     if(err) console.log(err);
   });
@@ -41,19 +40,15 @@ router.delete('/:id', auth.loggedIn, auth.hasDocument, function(req, res) {
   {
     if(req.user.documents[i]._id == req.doc._id)
     {
-      console.log(i);
       //remove from index i
       req.user.documents.splice(i, 1);
     }
   }
-  console.log("req.doc._id", req.doc._id);
   req.user.markModified('documents');
   req.user.save();
   Documents.findByIdAndDelete(req.doc._id, function(err,data) {
     if(err) console.log(err);
-    console.log("data from findByIdAndDelete callback:", data);
   });
-  console.log("finished");
   res.send("success");
 });
 
@@ -62,7 +57,8 @@ router.get('/', auth.loggedIn, function(req,res) {
   res.json(req.user.documents);
 });
 router.post('/', auth.loggedIn, function(req, res) {
-  res.send(createDocument(req.user, "Untitled Document", [], Date.now()));
+  var doc_id = createDocument(req.user, "Untitled Document", [], Date.now());
+  res.send(doc_id);
 });
 router.put('/', auth.loggedIn, function(req, res) {
   res.send(createDocument(req.user, req.body.title, req.body.text, Date.now()));
@@ -71,8 +67,6 @@ router.put('/', auth.loggedIn, function(req, res) {
 //send to guide of sessionid
 router.post('/:id/send', auth.loggedIn, auth.hasDocument, function(req, res) {
   Sessions.findById(req.body.sessionid, function(err, session) {
-    console.log(req.doc);
-    console.log(session);
     Users.findById(session.createdBy, function(err, user) {
       cloneDocument(user, req.doc);
     });
@@ -82,28 +76,25 @@ router.post('/:id/send', auth.loggedIn, auth.hasDocument, function(req, res) {
 //creates document and adds to user with title, text, and date
 //returns document id
 function createDocument(user, title, text, date) {
-  crypto.randomBytes(32, function(err, buffer) {
-    var aws_filename = buffer.toString("hex");
-    s3.upload({Bucket: "tutopoint-doc-bucket", Key: aws_filename, Body: JSON.stringify(text)}, function (err, data) {
-      if (err) {
-        console.log("Error", err);
-      } if (data) {
-        console.log("Upload Success", data.Location);
-      }
-    });
-
-    var newDoc = new Documents({
-      title: title,
-      date: date,
-      aws_filename: aws_filename,
-    });
-    newDoc.save(function(err, doc) {
-      user.documents.push(doc._id);
-      user.markModified('documents');
-      user.save();
-      return doc._id;
-    });
+  var buffer = crypto.randomBytes(32);
+  var aws_filename = buffer.toString("hex");
+  s3.upload({Bucket: "tutopoint-doc-bucket", Key: aws_filename, Body: JSON.stringify(text)}, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
   });
+
+  var newDoc = new Documents({
+    title: title,
+    date: date,
+    aws_filename: aws_filename,
+  });
+  user.documents.push(newDoc._id);
+  user.markModified('documents');
+  user.save();
+  newDoc.save();
+
+  return newDoc._id;
 }
 //clones document and adds to user
 function cloneDocument(user, document) {
