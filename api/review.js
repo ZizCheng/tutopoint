ca4acc429624798d0b019950099e344e27ced274
Zizheng Cheng <zizcheng@berkeley.edu>
2020-04-02 16:56:08 -0700

    some review stuff

---
+++ api/review.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const review = new express.Router();
+const auth = require('../auth/auth.js');
+const Referrals = require('../models/model.js').Referrals;
+const Guides = require('../models/model.js').Guides;
+
+const nodemailer = require('nodemailer');
+const mailAuth = require('../secret.js').mailAuth;
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: mailAuth,
+});
+
+review.use(auth.loggedIn);
+review.use(auth.ensureUserIsClient);
+
+review.post('/:id/newreview', function(req, res) {
+  stripe.customers.retrieve(req.user.stripeCustomerId, function(err, customer) {
+    Guides.findOne({_id: req.params.id})
+        .then((guide) => {
+          if (req.body.review) {
+            guide.comments.push(req.body.review);
+            guide.markModified('comments');
+          }
+          if (req.body.star != 0) {
+            guide.ratings.push(req.body.star);
+            guide.markModified('ratings');
+          }
+          guide.save();
+          res.send('success');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send('Internal Server Error.');
+        });
+  });
+});
+
+review.post('/refer', function(req, res) {
+  const profile = req.user.toJSON();
+  Referrals
+      .findOne({referrer: profile['_id']})
+      .select('code')
+      .then((Referral) => {
+        const email = req.body.email;
+        const name = req.user.name;
+        const mailOptions = {
+          from: 'TutoPoint <auth@tutopoint.com>',
+          to: email,
+          subject: '[TutoPoint] Invitation to Join',
+          text: 'Hello!\n\n ' +
+           name + ' has referred you to use TutoPoint, an online college consulting platform.\n' +
+           'TutoPoint connects clients like you with our pool of college student guides, allowing you to ask questions to real time college students.\n' +
+           'You can signup at anytime at this link: https://tutopoint.com/signup/' + Referral.code + '\n' +
+           'To visit our main page and see our products and services, please go to https://tutopoint.com \n' +
+           'For any questions, concerns, or requests, you can email support@tutopoint.com and we will reply within 2 business days.' +
+           '\n\nThank you for your interest, we hope to see you using our platform.\n' +
+           'Best, TutoPoint LLC.'
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.log(error);
+            reject(error);
+          } else {
+            console.log('Email sent1');
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err); res.send('Internal Server Error.');
+      });
+});
+
+review.post('/:id/report', function(req, res) {
+  Guides.findOne({_id: req.params.id})
+      .then((guide) => {
+        const name = guide.name;
+        const mailOptions = {
+          from: 'TutoPoint <auth@tutopoint.com>',
+          to: 'zizcheng@berkeley.edu',
+          subject: 'REPORT',
+          text: 'There has been a new report regarding guide ' + name + ', submitted by ' + req.user.name +
+          '. The user reported:\n\n' + req.body.report +
+          '\n\nThe client\'s email is ' + req.user.email + '\n' +
+          'The guide\'s email is ' + guide.email
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.log(error);
+            reject(error);
+          } else {
+            console.log('Email sent1');
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send('Internal Server Error.');
+      });
+});
+
+
+module.exports = review;
--- react/components/PostCall.jsx
+++ react/components/PostCall.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {Link} from 'react-router-dom';
+
 import "./PostCall.scss";
+import reviewAPI from "../api/review.js";
 
 class PostCall extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {guideName: null, guideID: null}
   }
 
   toggleReportModal() {
@@ -22,9 +26,29 @@ class PostCall extends React.Component {
     }
     else document.getElementById("recommend-modal").classList.add("is-active");
   }
+  goDashboard() {
+    window.location.href="/dashboard";
+  }
 
   componentDidMount() {
 
+  }
+
+  submitReview() {
+    reviewAPI.newReview(this.state.guideID, document.getElementById("review-box").value, 0);
+    window.location.href="/dashboard";
+  }
+
+  submitReport() {
+    reviewAPI.report(this.state.guideID, document.getElementById("report-box").value);
+    toggleReportModal();
+    document.getElementById("report-button").disabled = true;
+  }
+
+  submitInvite() {
+    reviewAPI.refer(document.getElementById("invite-box").value);
+    toggleRecommendModal();
+    document.getElementById("invite-button").disabled = true;
   }
 
   render() {
@@ -49,31 +73,36 @@ class PostCall extends React.Component {
               <div className="postcall-star postcall-star-5"><i className="fas fa-star"></i></div>
             </div>
             <div className="postcall-buttons-wrapper">
-              <button className="postcall-report-button button is-outlined" onClick={this.toggleReportModal}>Report</button>
-              <button className="postcall-recommend-button button is-primary is-outlined" onClick={this.toggleRecommendModal}>Recommend</button>
+              <button id="report-button" className="postcall-report-button button is-outlined" onClick={this.toggleReportModal}>Report</button>
+              <button id="invite-button" className="postcall-recommend-button button is-primary is-outlined" onClick={this.toggleRecommendModal}>Recommend</button>
             </div>
           </div>
           <div className="postcall-review-wrapper">
-            <textarea className="textarea" placeholder="Type your review here."></textarea>
+            <textarea className="textarea" id="review-box" placeholder="Type your review here."></textarea>
           </div>
-          <button className="postcall-submit-button button is-primary">Submit Review</button>
+          <button className="postcall-submit-button button is-primary" onClick={this.submitReview}>Submit Review</button>
           <div className="is-divider" data-content="or"></div>
-          <button className="postcall-back-button button is-dark">Back to Dashboard</button>
+          <button type="button" className="postcall-back-button button is-dark" onClick={this.goDashboard}>Back to Dashboard</button>
         </div>
+
         <div id="report-modal" className="modal">
           <div className="modal-background"  onClick={this.toggleReportModal}></div>
           <div className="modal-content">
             <h1 className="title">Report User</h1>
             <p>Sorry about that!</p>
-            <textarea className="textarea" placeholder="Tell us what went wrong."></textarea>
-            <button className="submit-button button is-primary">Submit</button>
+            <textarea className="textarea" id="report-box" placeholder="Tell us what went wrong."></textarea>
+            <button className="submit-button button is-primary" onClick={this.submitReport}>Submit</button>
           </div>
           <button className="modal-close is-large" aria-label="close" onClick={this.toggleReportModal}></button>
         </div>
+
         <div id="recommend-modal" className="modal">
           <div className="modal-background"  onClick={this.toggleRecommendModal}></div>
           <div className="modal-content">
-            <p>Email:</p><input className="recommend-email" type="email" />
+            <h1 className="title">Refer a friend!</h1>
+            <p>Would your friends like {}? Fill in an email below and we will send them an invite. You receive credit every time they have a session!</p>
+            <p>Email:</p><input className="recommend-email" id="invite-box" type="email" />
+            <button className="submit-button button is-primary" onClick={this.submitInvite}>Invite</button>
           </div>
           <button className="modal-close is-large" aria-label="close"  onClick={this.toggleRecommendModal}></button>
         </div>
